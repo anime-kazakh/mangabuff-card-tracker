4c453ceacf248a6fc4b259ae21bfd828ee42f324
Arman <golopichu@gmail.com>
2025-07-19 00:02:19 +0400

    add mvp bot

--- src/main.py
+++ src/main.py
@@ -2,22 +2,38 @@ from os import getenv
 import logging
 
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-from MangabuffParser import MangabuffParser, CardRank
-
+from MangabuffParser import MangabuffParser
+from resources.messages import *
+from src.MangabuffParser import CardInfo
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(START_MESSAGE)
+
+async def card(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mb_parser = MangabuffParser(
+        mail=getenv("MANGABUFF_MAIL"),
+        password=getenv("MANGABUFF_PASSWORD")
+    )
+    cards_list = list(mb_parser.get_cards_lots(want=True))
+    await update.message.reply_markdown(CardInfo.out_list(cards_list))
+
 def main():
-    logging.basicConfig(filename="mangabuff-card-tracker.log", level=logging.INFO)
+    logging.basicConfig(filename="../logs/mangabuff-card-tracker.log", level=logging.INFO, encoding="utf-8")
     logger.info("Starting mangabuff-card-tracker")
 
-    mail = getenv("MANGABUFF_MAIL")
-    password = getenv("MANGABUFF_PASSWORD")
-    mb_parser = MangabuffParser(mail=mail, password=password)
-    print(mb_parser.get_cards_lots(query='Восхождение в тени', want=True, rank=CardRank.D))
+    app = ApplicationBuilder().token(getenv("BOT_TOKEN")).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("card", card))
+
+    app.run_polling()
 
     logger.info(f"Finished mangabuff-card-tracker")
 
