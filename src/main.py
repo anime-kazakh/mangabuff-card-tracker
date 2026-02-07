db8ed513752b9d3cb4e0b17355501e96284643d9
Arman <golopichu@gmail.com>
2026-02-08 02:26:09 +0400

    Исправил баги с библиотеками resources и пару маленьких исправление DEBUG mode и print на начало и конец скрипта

--- src/main.py
+++ src/main.py
@@ -1,25 +1,31 @@
 from pathlib import Path
-from os import getenv
+from os import getenv, makedirs
 import logging
 from datetime import time
-
-from dotenv import load_dotenv
 
 from TrackerBot import TrackerBot
 from MangabuffParser import MangabuffParser
 
 
-logger = logging.getLogger(__name__)
+# ------------------- ENV - for debug mode ----------------------
+DEBUG = getenv("DEBUG", "False").lower().strip() in ('true', '1')
+DEBUG = True
+if DEBUG:
+    from dotenv import load_dotenv
+    load_dotenv()
+# ---------------------------------------------------------------
 
-load_dotenv()
+logger = logging.getLogger(__name__)
 
 PROJECT_ROOT = Path(__file__).parent.parent.resolve()
 
 LOG_FORMAT = "%(asctime)s:%(levelname)s:%(name)s - %(message)s"
 
 def main():
-    log_file_path = PROJECT_ROOT / "logs" / "mangabuff-card-tracker.log"
-    logging.basicConfig(filename=log_file_path, format=LOG_FORMAT, level=logging.INFO, encoding="utf-8")
+    log_file_path = PROJECT_ROOT / "logs"
+    makedirs(log_file_path, exist_ok=True)
+    log_file_name = "mangabuff-card-tracker.log"
+    logging.basicConfig(filename=log_file_path / log_file_name, format=LOG_FORMAT, level=logging.INFO, encoding="utf-8")
     logger.info("Starting mangabuff-card-tracker")
 
     parser = MangabuffParser(
@@ -33,13 +39,17 @@ def main():
         parser=parser,
         timestamps=[
             time(11,0,0),
-            time(15, 0, 0)
+            time(15,0,0)
         ]
     )
+
+    print('START - MangaBuff Card Tracker Bot')
 
     tracker.run()
 
     logger.info(f"Finished mangabuff-card-tracker")
+
+    print('STOP - MangaBuff Card Tracker Bot')
 
 
 if __name__ == '__main__':
